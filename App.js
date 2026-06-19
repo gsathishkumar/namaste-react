d2046a72b4f459f38b2350160e62ffe88c5dc2e3
Sathishkumar <be.sathish@gmail.com>
2026-06-19 15:03:58 +0530

    Episode-01

---
+++ App.js
@@ -0,0 +1,20 @@
+// const heading = React.createElement(
+//   'h1',
+//   { id: 'heading' },
+//   'Hello World from React',
+// );
+
+const parent = React.createElement('div', { id: 'parent' }, [
+  React.createElement('div', { id: 'child1' }, [
+    React.createElement('h1', {}, 'I am an H1 Tag'),
+    React.createElement('h2', {}, 'I am an H2 Tag'),
+  ]),
+  React.createElement('div', { id: 'child2' }, [
+    React.createElement('h1', {}, 'I am an H1 Tag'),
+    React.createElement('h2', {}, 'I am an H2 Tag'),
+  ]),
+]);
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+console.log(parent);
+root.render(parent);
