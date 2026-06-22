71f0446f8320a5ecac00c32436f74080c71d02e5
Sathishkumar <be.sathish@gmail.com>
2026-06-22 13:56:54 +0530

    Episode-3 Component Composition Multiple Ways

--- App.js
+++ App.js
@@ -3,14 +3,22 @@ import ReactDOM from 'react-dom/client';
 
 const jsxHeading = <h1 id="heading">JSX Heading</h1>;
 
-const Title = () => <h3 id="title">Title</h3>;
+const TitleComponent = () => <h3 id="title">Title Component</h3>;
+
+const TitleNormalFunction = function () {
+  return <h3 id="title">Title Normal Function</h3>;
+};
+
+const TitleElement = <h3 id="title">Title Element</h3>;
 
 const HeadingComponent = () => {
   return (
     <div id="container">
       <h1>Component Composition</h1>
       <h2 id="heading">Heading</h2>
-      <Title />
+      {TitleElement}
+      {TitleNormalFunction()}
+      <TitleComponent />
     </div>
   );
 };
